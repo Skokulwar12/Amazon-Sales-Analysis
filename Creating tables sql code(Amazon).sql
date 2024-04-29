82e881e3b336fb26b676084d44d739f8f1f82bbf
Srushti Kokulwar <163553184+Skokulwar12@users.noreply.github.com>
2024-04-29 16:27:52 +0530

    Update Creating tables sql code(Amazon).sql

--- Creating tables sql code(Amazon).sql
+++ Creating tables sql code(Amazon).sql
@@ -1,54 +1,67 @@
--- Creating sellers table
+-- first import into products,
+-- import into customers
+-- import into sellers
+-- import into orders
+-- import into returns
+
+
+-- creating customers table
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers (
+                            customer_id VARCHAR(25) PRIMARY KEY,
+                            customer_name VARCHAR(25),
+                            state VARCHAR(25)
+);
+
+
+-- creating sellers table
 DROP TABLE IF EXISTS sellers;
 CREATE TABLE sellers (
-						seller_id VARCHAR(10),	
-						seller_name VARCHAR(25)
-					); 
+                        seller_id VARCHAR(25) PRIMARY KEY,
+                        seller_name VARCHAR(25)
+);
 
 
--- Creating products table
+-- creating products table
 DROP TABLE IF EXISTS products;
 CREATE TABLE products (
-                        product_id VARCHAR(10),    
-                        product_name VARCHAR(255),    
-                        price FLOAT,    
+                        product_id VARCHAR(25) PRIMARY KEY,
+                        product_name VARCHAR(255),
+                        Price FLOAT,
                         cogs FLOAT
-                        );
+);
 
 
 
-
--- Creating customers table 
-DROP TABLE IF EXISTS customers;
-CREATE TABlE customers (
-						customer_id VARCHAR(10),	
-						customer_name VARCHAR(55),	
-						state VARCHAR(55)
-						);
-
-
-
--- Creating orders table 
+-- creating orders table
 DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
-						order_id VARCHAR(10),	
-						order_date DATE	,
-						customer_id	VARCHAR(10),
-						state	VARCHAR(30),
-						category VARCHAR(55),	
-						sub_category VARCHAR(155),	
-						product_id	VARCHAR(25),
-						price_per_unit	FLOAT,
-						quantity INT,	
-						sale	FLOAT,
-						seller_id 	VARCHAR(10)
-					);
+                        order_id VARCHAR(25) PRIMARY KEY,
+                        order_date DATE,
+                        customer_id VARCHAR(25),  -- this is a foreign key from customers(customer_id)
+                        state VARCHAR(25),
+                        category VARCHAR(25),
+                        sub_category VARCHAR(25),
+                        product_id VARCHAR(25),   -- this is a foreign key from products(product_id)
+                        price_per_unit FLOAT,
+                        quantity INT,
+                        sale FLOAT,
+                        seller_id VARCHAR(25),    -- this is a foreign key from sellers(seller_id)
+    
+                        CONSTRAINT fk_customers FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+                        CONSTRAINT fk_products FOREIGN KEY (product_id) REFERENCES products(product_id),    
+                        CONSTRAINT fk_sellers FOREIGN KEY (seller_id) REFERENCES sellers(seller_id)
+);
 
---Creating Returns Table
-DROP TABLE IF Exists returns;
+
+
+-- creating returns table
+DROP TABLE IF EXISTS returns;
 CREATE TABLE returns (
-						order_id VARCHAR(10),
-						return_id VARCHAR(10)
-					); 
+                        order_id VARCHAR(25),
+                        return_id VARCHAR(25),
+                        CONSTRAINT pk_returns PRIMARY KEY (order_id), -- Primary key constraint
+                        CONSTRAINT fk_orders FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
 
 
